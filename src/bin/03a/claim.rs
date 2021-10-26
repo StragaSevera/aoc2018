91e809dcc0b38e88a35e46bce673fecda8ea43e5
StragaSevera <ought@yandex.ru>
2021-10-26 15:07:36 +0300

    Refactor to simpler error reporting

--- src/bin/03a/claim.rs
+++ src/bin/03a/claim.rs
@@ -2,36 +2,14 @@ use std::{
     fmt::{Display, Formatter},
     error,
     num::ParseIntError,
-    str::FromStr
+    str::FromStr,
 };
 use lazy_static::lazy_static;
 use regex::{Captures, Regex};
-use ClaimError::{FormatError, ParseError};
+use ClaimError::{FormatError};
 
-#[derive(Debug, Clone)]
-pub enum ClaimError {
-    FormatError,
-    ParseError(ParseIntError),
-}
-
-impl Display for ClaimError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            FormatError => { write!(f, "Wrong line format") }
-            ParseError(err) => { Display::fmt(&err, f) }
-        }
-    }
-}
-
-impl error::Error for ClaimError {}
-
-impl From<ParseIntError> for ClaimError {
-    fn from(error: ParseIntError) -> Self {
-        ParseError(error)
-    }
-}
-
-struct Claim {
+#[derive(PartialEq, Eq, Debug)]
+pub struct Claim {
     id: u32,
     x: u32,
     y: u32,
@@ -58,18 +36,62 @@ impl FromStr for Claim {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let caps = CLAIM_REGEX.captures(s).ok_or(FormatError)?;
-        Ok(Claim::new(caps)?)
+        Ok(Claim::new(caps))
     }
 }
 
 impl Claim {
-    fn new(caps: Captures) -> Result<Claim, ParseIntError> {
-        Ok(Self {
-            id: caps["id"].parse()?,
-            x: caps["x"].parse()?,
-            y: caps["y"].parse()?,
-            width: caps["width"].parse()?,
-            height: caps["height"].parse()?,
-        })
+    fn new(caps: Captures) -> Claim {
+        Self {
+            id: caps["id"].parse().unwrap(),
+            x: caps["x"].parse().unwrap(),
+            y: caps["y"].parse().unwrap(),
+            width: caps["width"].parse().unwrap(),
+            height: caps["height"].parse().unwrap(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ClaimError {
+    FormatError
+}
+
+impl Display for ClaimError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FormatError => { write!(f, "Wrong line format") }
+        }
+    }
+}
+
+impl error::Error for ClaimError {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const CORRECT_INPUT: &str = "#1 @ 2,3: 5x4";
+
+    #[test]
+    fn from_str_correct_test() {
+        let expected = Claim {
+            id: 1,
+            x: 2,
+            y: 3,
+            width: 5,
+            height: 4,
+        };
+        let result = CORRECT_INPUT.parse::<Claim>().unwrap();
+        assert_eq!(result, expected);
+    }
+
+    const INCORRECT_FORMAT_INPUT: &str = "@1 # 2,3: 5x4";
+
+    #[test]
+    fn from_str_incorrect_format_test() {
+        let expected = FormatError;
+        let result = INCORRECT_FORMAT_INPUT.parse::<Claim>().unwrap_err();
+        assert_eq!(result, expected);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Lines, Result};
 use std::iter::Map;
 
-pub fn read_file(filename: &str) -> Map<Lines<BufReader<File>>, fn(Result<String>) -> String> {
+type FileIterator = Map<Lines<BufReader<File>>, fn(Result<String>) -> String>;
+
+pub fn read_file(filename: &str) -> FileIterator {
     let file = File::open(filename).unwrap();
     let lines = BufReader::new(file).lines();
     lines.map(|line| line.unwrap())
