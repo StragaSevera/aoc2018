d85b1d631a17f8bcb71c3599de5ecc21689793d0
StragaSevera <ought@yandex.ru>
2021-10-27 13:21:38 +0300

    Solve 03a

--- src/bin/03a/claim.rs
+++ src/bin/03a/claim.rs
@@ -1,20 +1,22 @@
 use std::{
     fmt::{Display, Formatter},
     error,
-    num::ParseIntError,
     str::FromStr,
 };
+use std::cmp::{max, min};
 use lazy_static::lazy_static;
 use regex::{Captures, Regex};
 use ClaimError::{FormatError};
 
+type Square = (u32, u32);
+
 #[derive(PartialEq, Eq, Debug)]
 pub struct Claim {
     id: u32,
-    x: u32,
-    y: u32,
-    width: u32,
-    height: u32,
+    x1: u32,
+    y1: u32,
+    x2: u32,
+    y2: u32,
 }
 
 // #1 @ 1,3: 4x4
@@ -31,24 +33,63 @@ lazy_static! {
     ).unwrap();
 }
 
+impl Claim {
+    pub fn new(s: &str) -> Self {
+        s.parse().unwrap()
+    }
+
+    fn from_caps(caps: Captures) -> Claim {
+        let id = caps["id"].parse().unwrap();
+        let x1 = caps["x"].parse().unwrap();
+        let y1 = caps["y"].parse().unwrap();
+        let width: u32 = caps["width"].parse().unwrap();
+        let height: u32 = caps["height"].parse().unwrap();
+        Self {
+            id,
+            x1,
+            y1,
+            x2: x1 + width - 1,
+            y2: y1 + height - 1,
+        }
+    }
+
+    pub fn intersection(&self, other: &Claim) -> Option<Claim> {
+        if (self.x1 > other.x2) || (other.x1 > self.x2) ||
+            (self.y1 > other.y2) || (other.y1 > self.y2) {
+            return None;
+        }
+        Some(Self {
+            id: 0,
+            x1: max(self.x1, other.x1),
+            y1: max(self.y1, other.y1),
+            x2: min(self.x2, other.x2),
+            y2: min(self.y2, other.y2),
+        })
+    }
+
+    fn area(&self) -> u32 {
+        let width = self.x2 - self.x1 + 1;
+        let height = self.y2 - self.y1 + 1;
+        width * height
+    }
+
+    pub fn squares(&self) -> Vec<Square> {
+        let mut result = Vec::with_capacity(self.area() as usize);
+        for i in self.x1..=self.x2 {
+            for j in self.y1..=self.y2 {
+                result.push((i, j));
+            }
+        }
+        result
+    }
+}
+
 impl FromStr for Claim {
     type Err = ClaimError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let caps = CLAIM_REGEX.captures(s).ok_or(FormatError)?;
-        Ok(Claim::new(caps))
-    }
-}
-
-impl Claim {
-    fn new(caps: Captures) -> Claim {
-        Self {
-            id: caps["id"].parse().unwrap(),
-            x: caps["x"].parse().unwrap(),
-            y: caps["y"].parse().unwrap(),
-            width: caps["width"].parse().unwrap(),
-            height: caps["height"].parse().unwrap(),
-        }
+        Ok(Claim::from_caps(caps))
     }
 }
 
@@ -70,19 +111,20 @@ impl error::Error for ClaimError {}
 #[cfg(test)]
 mod tests {
     use super::*;
+    use pretty_assertions::assert_eq;
 
     const CORRECT_INPUT: &str = "#1 @ 2,3: 5x4";
 
     #[test]
     fn from_str_correct_test() {
+        let result = CORRECT_INPUT.parse::<Claim>().unwrap();
         let expected = Claim {
             id: 1,
-            x: 2,
-            y: 3,
-            width: 5,
-            height: 4,
+            x1: 2,
+            y1: 3,
+            x2: 6,
+            y2: 6,
         };
-        let result = CORRECT_INPUT.parse::<Claim>().unwrap();
         assert_eq!(result, expected);
     }
 
@@ -90,8 +132,66 @@ mod tests {
 
     #[test]
     fn from_str_incorrect_format_test() {
-        let expected = FormatError;
         let result = INCORRECT_FORMAT_INPUT.parse::<Claim>().unwrap_err();
+        assert_eq!(result, FormatError);
+    }
+
+    const INTERSECTION_SOME: [&str; 2] = [
+        "#1 @ 1,3: 4x4",
+        "#2 @ 3,1: 4x4"
+    ];
+
+    #[test]
+    fn intersection_some_test() {
+        let a = Claim::new(INTERSECTION_SOME[0]);
+        let b = Claim::new(INTERSECTION_SOME[1]);
+        let result = a.intersection(&b);
+
+        let expected = Some(Claim {
+            id: 0,
+            x1: 3,
+            y1: 3,
+            x2: 4,
+            y2: 4,
+        });
         assert_eq!(result, expected);
+    }
+
+    const INTERSECTION_NONE: [&str; 2] = [
+        "#1 @ 1,3: 4x4",
+        "#3 @ 5,5: 2x2"
+    ];
+
+    #[test]
+    fn intersection_none_test() {
+        let a = Claim::new(INTERSECTION_NONE[0]);
+        let b = Claim::new(INTERSECTION_NONE[1]);
+        let result = a.intersection(&b);
+
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn area_test() {
+        let claim = Claim::new(CORRECT_INPUT);
+        let result = claim.area();
+
+        assert_eq!(result, 20)
+    }
+
+    const SQUARES_INPUT: &str = "#1 @ 2,3: 2x3";
+    lazy_static! {
+        static ref SQUARES_RESULT: Vec<Square> = vec!(
+            (2, 3), (2, 4), (2, 5), (3, 3), (3, 4), (3, 5)
+        );
+    }
+
+    #[test]
+    fn squares_test() {
+        let claim = Claim::new(SQUARES_INPUT);
+        let result = claim.squares();
+
+        assert_eq!(result.len(), SQUARES_RESULT.len());
+        result.iter().zip(SQUARES_RESULT.iter()).for_each(|(a, b)| assert_eq!(a, b));
     }
 }
--- src/bin/03a/main.rs
+++ src/bin/03a/main.rs
@@ -1,30 +1,44 @@
 mod claim;
 
+use std::collections::HashSet;
+use claim::Claim;
+
 fn main() {
     let input = aoc2018::read_file("src/bin/03a/input.txt");
     let result = calculate(input);
     println!("{}", result)
 }
 
-fn calculate(input: impl Iterator<Item=String>) -> i32 {
-    5
+fn calculate(input: impl Iterator<Item=String>) -> usize {
+    let claims = make_claims(input);
+    let mut intersecting_squares = HashSet::new();
+    for i in 1..claims.len() {
+        for j in 0..i {
+            if let Some(intersection) = claims[i].intersection(&claims[j]) {
+                intersecting_squares.extend(intersection.squares());
+            }
+        }
+    }
+    intersecting_squares.len()
+}
+
+fn make_claims(input: impl Iterator<Item=String>) -> Vec<Claim> {
+    input.map(|s| Claim::new(&s)).collect()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use pretty_assertions::assert_eq;
 
-    const INPUT: &str = "abcde
-fghij
-klmno
-pqrst
-fguij
-axcye
-wvxyz";
+    const INPUT: &str = "\
+#1 @ 1,3: 4x4
+#2 @ 3,1: 4x4
+#3 @ 5,5: 2x2";
 
     #[test]
     fn calculate_test() {
         let result = calculate(INPUT.split('\n').map(String::from));
-        assert_eq!(result, 5)
+        assert_eq!(result, 4)
     }
 }
